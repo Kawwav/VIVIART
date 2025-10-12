5a8ea8c858d99ca364ee1908f979510a706f3cb2
Vini_Kawwa <102892179+Kawwav@users.noreply.github.com>
2025-10-12 12:44:38 -0300

    Add files via upload
    Adicionado a função MODAL onde clicamos em uma das bolsas na coleção e abre uma janela com descrição da bolsa

--- script.js
+++ script.js
@@ -42,3 +42,36 @@ prevBtn.addEventListener('click', () => {
 });
 
 window.addEventListener('resize', updateCarousel);
+
+
+function abrirModal(elemento) {
+  const modal = document.getElementById("modal");
+  const imgAmpliada = document.getElementById("imgAmpliada");
+  const modalTitulo = document.getElementById("modalTitulo");
+  const modalDescricao = document.getElementById("modalDescricao");
+  const modalPreco = document.getElementById("modalPreco");
+
+  // adiciona as informações da imagem
+  imgAmpliada.src = elemento.src;
+
+  // Exemplo de dados personalizados
+  if (elemento.alt === "bolsa1") {
+    modalTitulo.textContent = "Bolsa e porta celular";
+    modalDescricao.textContent = "Bolsa feita a mão e porta celular feita a mão com cores diferesnte ";
+    modalPreco.textContent = "R$250,00";
+  } else if (elemento.alt === "bolsa2") {
+    modalTitulo.textContent = "Bolsa artesanal azul";
+    modalDescricao.textContent = "bolsa azul combinando com roupas e acessorios azuis";
+    modalPreco.textContent = "R$280,00";
+  } else if (elemento.alt === "bolsa3") {
+    modalTitulo.textContent = "Bolsa artesanal marrom";
+    modalDescricao.textContent = "feita para diversões na praia ou climas limpos e claros";
+    modalPreco.textContent = "R$300,00";
+  }
+
+  modal.style.display = "flex";
+}
+
+function fecharModal() {
+  document.getElementById("modal").style.display = "none";
+}
